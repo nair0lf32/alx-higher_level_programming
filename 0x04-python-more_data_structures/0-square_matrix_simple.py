8a3d2407aa1c8deb881885f1537c94fcc3e8765a
nair0lf32 <florianedem@gmail.com>
2022-06-09 17:30:05 +0100

    fix task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     arr = []
     for i in matrix:
-        arr.append(list(map(lambda x: x * x, row)))
+        arr.append(list(map(lambda j: j * j, i)))
     return arr
