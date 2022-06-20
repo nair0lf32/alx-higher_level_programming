1eca908d4bab08c5be50dc26e7b1afe63e1a9de2
nair0lf32 <florianedem@gmail.com>
2022-06-20 17:52:11 +0100

    fixing task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -9,19 +9,6 @@ void print_python_float(PyObject *p);
 */
 void print_python_list(PyObject *p):
 {
-Py_ssize_t size, alloc, i;
-const char *type;
-PyListObject *list = (PyListObject *)p;
-PyVarObject *var = (PyVarObject *)p;
-size = var->ob_size;
-alloc = list->allocated;
-type = var->ob_type->tp_name;
-printf("%s size: %zd, alloc: %zd\n", type, size, alloc);
-for (i = 0; i < size; i++)
-{
-printf("[%d] ", i);
-print_python_object(list->ob_item[i]);
-}
 }
 /**
 *print_python_bytes - prints basic python bytes info
@@ -30,19 +17,6 @@ print_python_object(list->ob_item[i]);
 */
 void print_python_bytes(PyObject *p):
 {
-Py_ssize_t size, alloc, i;
-const char *type;
-PyBytesObject *bytes = (PyBytesObject *)p;
-PyVarObject *var = (PyVarObject *)p;
-size = var->ob_size;
-alloc = bytes->ob_alloc;
-type = var->ob_type->tp_name;
-printf("%s size: %zd, alloc: %zd\n", type, size, alloc);
-for (i = 0; i < size; i++)
-{
-printf("[%d] ", i);
-printf("%c", bytes->ob_sval[i]);
-}
 }
 /**
 *print_python_float - prints basic python float info
@@ -51,17 +25,4 @@ printf("%c", bytes->ob_sval[i]);
 */
 void print_python_float(PyObject *p):
 {
-Py_ssize_t size, alloc, i;
-const char *type;
-PyFloatObject *float_obj = (PyFloatObject *)p;
-PyVarObject *var = (PyVarObject *)p;
-size = var->ob_size;
-alloc = float_obj->ob_alloc;
-type = var->ob_type->tp_name;
-printf("%s size: %zd, alloc: %zd\n", type, size, alloc);
-for (i = 0; i < size; i++)
-{
-printf("[%d] ", i);
-printf("%f", float_obj->ob_fval);
-}
 }
