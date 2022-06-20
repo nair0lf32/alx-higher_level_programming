b3ea806dc319e96c7f8e0fa1b2da8f82f2d3a998
nair0lf32 <florianedem@gmail.com>
2022-06-20 14:01:20 +0100

    do task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            result.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            result.append(None)
+        except TypeError:
+            result.append(None)
+    return result
