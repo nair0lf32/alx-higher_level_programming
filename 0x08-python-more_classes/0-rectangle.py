dd42c3da8c46fbf51a3eaa0657afc99cf24d955c
nair0lf32 <florianedem@gmail.com>
2022-06-27 20:51:59 +0100

    fix task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""class Rectangle"""
+"""class Rectangle."""
 
 
 class Rectangle:
-    """A Rectangle"""
-    def __init__(self):
-        """Initializes the rectangle
-        """
-        pass
+    """A Rectangle."""
+
+    pass
