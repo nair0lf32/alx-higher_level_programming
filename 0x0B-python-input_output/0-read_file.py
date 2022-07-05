e3dcf19a5808b68870be2f0f293afe5fca68a18b
nair0lf32 <florianedem@gmail.com>
2022-07-05 17:00:56 +0100

    do task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""reads"""
+
+
+def read_file(filename=""):
+    """reads a file"""
+    with open(filename, 'r') as f:
+        print(f.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,3 @@
+# 0x0B-python-input_output
+
+automation go brrrrr
