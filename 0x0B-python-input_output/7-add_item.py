9c979294b3cce9e8bb1e752dcfda0241a34ead56
nair0lf32 <florianedem@gmail.com>
2022-07-05 18:36:52 +0100

    fixing task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""read add and save"""
-
-
-import sys
+"""read add then save to json file"""
 import json
+import sys
 import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__ == "__main__":
-    filename = "add_item.json"
-    my_list = []
-    if os.path.exists(filename):
-        my_list = load_from_json_file(filename)
+def read_add_save():
+    """adds all arguments to a Python list,
+        and then save them to a file"""
 
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, filename)
+    filename = "add_item.json"
+    args_list = []
+    if os.path.exists(filename):
+        args_list = load_from_json_file(filename)
+
+    args_list.extend(sys.argv[1:])
+    save_to_json_file(args_list, filename)
+
+
+if __name__ == "__main__":
+    read_add_save()
