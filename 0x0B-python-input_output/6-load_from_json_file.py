952d8169069870b27ec3f41e0f8747310031ac4b
nair0lf32 <florianedem@gmail.com>
2022-07-05 17:44:06 +0100

    do task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""read json from a file"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates a string object from a json file"""
+    with open(filename, "r", encoding="utf-8") as f:
+        json.load(f)
