8dc5b5a20af1dda06b2b1176e824d318ad8a7b81
nair0lf32 <florianedem@gmail.com>
2022-07-05 18:34:22 +0100

    fix task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""read add then save to json file"""
-import json
+
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-import os.path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-
-def read_add_save():
-    """adds all arguments to a Python list,
-        and then save them to a file"""
-
-    filename = "add_item.json"
-    args_list = []
-    if os.path.exists(filename):
-        args_list = load_from_json_file(filename)
-
-    args_list.extend(sys.argv[1:])
-    save_to_json_file(args_list, filename)
-
 
 if __name__ == "__main__":
-    read_add_save()
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
