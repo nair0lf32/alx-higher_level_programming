6281e08707d624eb8278fa44da52babed63ee570
nair0lf32 <florianedem@gmail.com>
2022-09-10 13:44:52 +0100

    fix task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """Lists all states from the database hbtn_0e_0_usa"""
-
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     user = sys.argv[1]
     pwd = sys.argv[2]
     db = sys.argv[3]
-    conn = MySQLdb.connect(user, pwd, db)
+    conn = MySQLdb.connect(user=user, passwd=pwd, db=db)
     csr = conn.cursor()
-    csr.execute("SELECT * FROM 'states'")
+    csr.execute("SELECT * FROM states ORDER BY states.id ASC")
     for state in csr.fetchall():
         print(state)
