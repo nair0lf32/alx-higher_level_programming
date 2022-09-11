d7377bbbb0ba01d1c213aca945da2dffc41a4bed
nair0lf32 <florianedem@gmail.com>
2022-09-11 15:40:42 +0100

    fix task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,8 +15,9 @@ if __name__ == "__main__":
         sys.argv[3]),
         pool_pre_ping=True)
     session = Session(bind=engine)
-    for state_obj in session.query(State):
-        if state_obj.name == sys.argv[4]:
+    for state_obj in \
+            session.query(State).filter(State.name == sys.argv[4]).first():
+        if state_obj.name is not None:
             print("{}".format(state_obj.id))
             break
         else:
