97b5c11b583b61fe1d20ebb2a5640a5141937129
nair0lf32 <florianedem@gmail.com>
2022-09-11 15:05:09 +0100

    fixing task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_0_usa"""
+"""Lists all cities from the database hbtn_0e_0_usa
+matching input state name"""
 import sys
 import MySQLdb
 
