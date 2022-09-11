4d271dea28fc68bd8a05ccec8b3a280cf5cdb14a
nair0lf32 <florianedem@gmail.com>
2022-09-11 14:41:30 +0100

    fix task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(user=user, passwd=pwd, db=db)
     csr = conn.cursor()
     csr.execute("SELECT * FROM cities INNER JOIN states \
-        ON cities.state_id = states.id ORDER BY states.id ASC")
+        ON cities.state_id = states.id ORDER BY cities.id ASC")
     for city in csr.fetchall():
         if city[4] == sys.argv[4]:
             print(", ".join(city[2]))
