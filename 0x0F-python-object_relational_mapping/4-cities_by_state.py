d05b4f94f0d22bf2af37950202751c03fa51fadf
nair0lf32 <florianedem@gmail.com>
2022-09-11 13:42:41 +0100

    do task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Lists all states starting with 'N' from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    conn = MySQLdb.connect(user=user, passwd=pwd, db=db)
+    csr = conn.cursor()
+    csr.execute("SELECT citiies.id, cities.name, states.name \
+        FROM cities JOIN states \
+        ON cities.state_id ORDER BY cities.id ASC")
+    for city in csr.fetchall():
+        print(city)
